f5506a71a4e0a45d385e59397c9ecff48bd23634
fecrakib <rakibislam132575@gmail.com>
2024-05-21 10:58:16 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+
+
+import express, { Request, Response } from 'express'
+
+const app = express()
+app.use(express.json())
+
+
+
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello World dfd!')
+})
+
+
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+
+async function main() {
+  try {
+    if (!config.db_url) {
+      throw new Error('DB_URL is not defined');
+    }
+    await mongoose.connect(config.db_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+main();
