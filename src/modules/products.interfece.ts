d2f8b57ae8c7f624e09d71d88069e7b0a2a97d03
fecrakib <rakibislam132575@gmail.com>
2024-05-21 11:07:46 +0600

    interface complete

---
+++ src/modules/products.interfece.ts
@@ -0,0 +1,24 @@
+
+
+// Define a type for a variant
+export type TVariant = {
+    type: string;
+    value: string;
+};
+
+// Define a type for inventory details
+export type TInventory = {
+    quantity: number;
+    inStock: boolean;
+};
+
+// Define a type for the main product
+export type TProduct = {
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    tags: string[];
+    variants: TVariant[];
+    inventory: TInventory;
+};
